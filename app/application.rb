44e63ed42ee6b7e645f24bf11026efa5e99b6127
Jon Mackey <jon@jonmackey.party>
2019-02-10 17:16:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path=="/items/"
       resp.write "#{item.price}"
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     end
  
