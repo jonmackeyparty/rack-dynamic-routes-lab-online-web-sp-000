e8d8a9436b9d6453a2b9ebaf30cf0e404e6d702f
Jon Mackey <jon@jonmackey.party>
2019-02-10 17:21:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       
       
-      resp.write "#{item.price}"
+      resp.write item.price
     else
       resp.write "Item not found"
       resp.status = 404
