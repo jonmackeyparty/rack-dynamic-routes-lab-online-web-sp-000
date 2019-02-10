799f74de6b31526ffb5a92342246b36f1d8825c2
Jon Mackey <jon@jonmackey.party>
2019-02-10 17:24:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,15 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
+      if item != nil 
+        resp.write item.price
+      else 
+        resp.write "Item not found"
+        resp.status = 400
+        
       
       
       
-      resp.write item.price
     else
       resp.write "Item not found"
       resp.status = 404
