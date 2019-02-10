0583032b0fc0c873a43731aa665846bbfcd34059
Jon Mackey <jon@jonmackey.party>
2019-02-10 17:18:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
     if req.path=="/items/"
       item_name = req.path.split("/item/").last
       item = @@items.find{|i| i.name == item_name}
+      
       resp.write "#{item.price}"
     else
       resp.write "Item not found"
